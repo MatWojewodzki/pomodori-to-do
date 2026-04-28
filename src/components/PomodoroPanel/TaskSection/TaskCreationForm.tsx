c59d230052e7fddbaa67fa2c8c23e2a355e4f679
Mateusz Wojewódzki <matwojewodzki@gmail.com>
2026-04-28 21:55:44 +0200

    refactor: extract TaskForm component from TaskCreationForm

--- src/components/PomodoroPanel/TaskSection/TaskCreationForm.tsx
+++ src/components/PomodoroPanel/TaskSection/TaskCreationForm.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useRef, useState } from 'react'
-import classNames from 'classnames'
+import { useState } from 'react'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import taskApi from '../../../api/task.ts'
+import TaskForm from './TaskForm.tsx'
 
 type TaskCreationFormProps = {
     closeForm: () => void
@@ -10,7 +10,6 @@ type TaskCreationFormProps = {
 function TaskCreationForm(props: TaskCreationFormProps) {
     const [taskDescription, setTaskDescription] = useState('')
     const [totalPomodoriCount, setTotalPomodoriCount] = useState(1)
-    const taskDescriptionInputRef = useRef<HTMLInputElement>(null)
 
     const queryClient = useQueryClient()
     const mutation = useMutation({
@@ -22,111 +21,22 @@ function TaskCreationForm(props: TaskCreationFormProps) {
         },
     })
 
-    function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
-        e.preventDefault()
+    function handleSubmit() {
         if (taskDescription.trim() === '') return
         mutation.mutate({
             text: taskDescription.trim(),
             pomodoroTotal: totalPomodoriCount,
         })
     }
-
-    useEffect(() => {
-        taskDescriptionInputRef.current?.focus()
-    }, [])
-
     return (
-        <form
-            className="flex-1 mt-3 p-4 flex flex-col rounded-md bg-neutral-600"
-            onSubmit={handleSubmit}
-        >
-            <label
-                htmlFor="task-description-input"
-                className="text-sm text-neutral-200"
-            >
-                Task description
-            </label>
-            <input
-                id="task-description-input"
-                type="text"
-                className={classNames(
-                    'mt-1 px-2 py-2 rounded-md bg-neutral-700 ',
-                    'focus-visible:outline-2 outline-white'
-                )}
-                value={taskDescription}
-                onChange={(e) => setTaskDescription(e.target.value)}
-                ref={taskDescriptionInputRef}
-            />
-            <label
-                htmlFor="total-pomodori-count-spinbutton"
-                className="mt-6 ps-1 text-sm text-neutral-200"
-            >
-                Total pomodori to finish
-            </label>
-            <div className="mt-1 flex justify-start">
-                <div
-                    id="total-pomodori-count-spinbutton"
-                    role="spinbutton"
-                    aria-valuenow={totalPomodoriCount}
-                    aria-valuemin={1}
-                    aria-live="polite"
-                    className="flex rounded-md bg-neutral-700"
-                >
-                    <button
-                        aria-label="decrease"
-                        type="button"
-                        className={classNames(
-                            'px-4 py-2 text-xl font-bold rounded-s-md',
-                            'hover:bg-neutral-500 focus:outline-none focus-visible:bg-neutral-500'
-                        )}
-                        onClick={() => {
-                            if (totalPomodoriCount > 1)
-                                setTotalPomodoriCount(totalPomodoriCount - 1)
-                        }}
-                    >
-                        -
-                    </button>
-                    <span className="w-12 py-2 flex justify-center items-center ">
-                        {totalPomodoriCount}
-                    </span>
-                    <button
-                        aria-label="increase"
-                        type="button"
-                        className={classNames(
-                            'px-4 py-2 text-xl font-bold rounded-e-md',
-                            'hover:bg-neutral-500 focus:outline-none focus-visible:bg-neutral-500'
-                        )}
-                        onClick={() =>
-                            setTotalPomodoriCount(totalPomodoriCount + 1)
-                        }
-                    >
-                        +
-                    </button>
-                </div>
-            </div>
-            <div className="mt-6 flex justify-end gap-4">
-                <button
-                    type="button"
-                    onClick={props.closeForm}
-                    className={classNames(
-                        'px-4 py-1 text-sm rounded-sm border-2 border-white',
-                        'hover:bg-neutral-200 focus:outline-none focus-visible:bg-neutral-200',
-                        'hover:border-neutral-200 focus-visible:border-neutral-200 hover:text-black focus-visible:text-black'
-                    )}
-                >
-                    Cancel
-                </button>
-                <button
-                    type="submit"
-                    className={classNames(
-                        'px-4 py-1 text-sm rounded-sm bg-white text-black',
-                        'hover:bg-neutral-200 focus:outline-none focus-visible:bg-neutral-200'
-                    )}
-                >
-                    Add
-                </button>
-            </div>
-        </form>
+        <TaskForm
+            taskDescription={taskDescription}
+            setTaskDescription={setTaskDescription}
+            totalPomodoriCount={totalPomodoriCount}
+            setTotalPomodoriCount={setTotalPomodoriCount}
+            handleSubmit={handleSubmit}
+            handleCancel={props.closeForm}
+        />
     )
 }
 
---
+++ src/components/PomodoroPanel/TaskSection/TaskForm.tsx
@@ -0,0 +1,122 @@
+import { useEffect, useRef } from 'react'
+import classNames from 'classnames'
+
+type TaskFormProps = {
+    handleSubmit: () => void
+    handleCancel: () => void
+    taskDescription: string
+    setTaskDescription: (value: string) => void
+    totalPomodoriCount: number
+    setTotalPomodoriCount: (value: number) => void
+}
+
+function TaskForm(props: TaskFormProps) {
+    const taskDescriptionInputRef = useRef<HTMLInputElement>(null)
+
+    useEffect(() => {
+        taskDescriptionInputRef.current?.focus()
+    }, [])
+
+    return (
+        <form
+            className="flex-1 mt-3 p-4 flex flex-col rounded-md bg-neutral-600"
+            onSubmit={(e) => {
+                e.preventDefault()
+                props.handleSubmit()
+            }}
+        >
+            <label
+                htmlFor="task-description-input"
+                className="text-sm text-neutral-200"
+            >
+                Task description
+            </label>
+            <input
+                id="task-description-input"
+                type="text"
+                className={classNames(
+                    'mt-1 px-2 py-2 rounded-md bg-neutral-700 ',
+                    'focus-visible:outline-2 outline-white'
+                )}
+                value={props.taskDescription}
+                onChange={(e) => props.setTaskDescription(e.target.value)}
+                ref={taskDescriptionInputRef}
+            />
+            <label
+                htmlFor="total-pomodori-count-spinbutton"
+                className="mt-6 ps-1 text-sm text-neutral-200"
+            >
+                Total pomodori to finish
+            </label>
+            <div className="mt-1 flex justify-start">
+                <div
+                    id="total-pomodori-count-spinbutton"
+                    role="spinbutton"
+                    aria-valuenow={props.totalPomodoriCount}
+                    aria-valuemin={1}
+                    aria-live="polite"
+                    className="flex rounded-md bg-neutral-700"
+                >
+                    <button
+                        aria-label="decrease"
+                        type="button"
+                        className={classNames(
+                            'px-4 py-2 text-xl font-bold rounded-s-md',
+                            'hover:bg-neutral-500 focus:outline-none focus-visible:bg-neutral-500'
+                        )}
+                        onClick={() => {
+                            if (props.totalPomodoriCount > 1)
+                                props.setTotalPomodoriCount(
+                                    props.totalPomodoriCount - 1
+                                )
+                        }}
+                    >
+                        -
+                    </button>
+                    <span className="w-12 py-2 flex justify-center items-center ">
+                        {props.totalPomodoriCount}
+                    </span>
+                    <button
+                        aria-label="increase"
+                        type="button"
+                        className={classNames(
+                            'px-4 py-2 text-xl font-bold rounded-e-md',
+                            'hover:bg-neutral-500 focus:outline-none focus-visible:bg-neutral-500'
+                        )}
+                        onClick={() =>
+                            props.setTotalPomodoriCount(
+                                props.totalPomodoriCount + 1
+                            )
+                        }
+                    >
+                        +
+                    </button>
+                </div>
+            </div>
+            <div className="mt-6 flex justify-end gap-4">
+                <button
+                    type="button"
+                    onClick={props.handleCancel}
+                    className={classNames(
+                        'px-4 py-1 text-sm rounded-sm border-2 border-white',
+                        'hover:bg-neutral-200 focus:outline-none focus-visible:bg-neutral-200',
+                        'hover:border-neutral-200 focus-visible:border-neutral-200 hover:text-black focus-visible:text-black'
+                    )}
+                >
+                    Cancel
+                </button>
+                <button
+                    type="submit"
+                    className={classNames(
+                        'px-4 py-1 text-sm rounded-sm bg-white text-black',
+                        'hover:bg-neutral-200 focus:outline-none focus-visible:bg-neutral-200'
+                    )}
+                >
+                    Add
+                </button>
+            </div>
+        </form>
+    )
+}
+
+export default TaskForm
