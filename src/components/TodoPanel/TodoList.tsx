1709d95b193e61244d3ff4dff05687c0890ea570
Mateusz Wojewódzki <matwojewodzki@gmail.com>
2026-06-11 20:31:01 +0200

    refactor: split TodoList component into two separate parts

--- src/components/TodoPanel/TodoList.tsx
+++ src/components/TodoPanel/TodoList.tsx
@@ -1,30 +1,18 @@
-import { useQuery } from '@tanstack/react-query'
-import todoService from '../../services/tauri/todo.ts'
 import TodoListItem from './TodoListItem.tsx'
-import ErrorMessage from '../common/ErrorMessage.tsx'
+import { TodoDto } from '../../types/generated/TodoDto.ts'
 
-function TodoList() {
-  const result = useQuery({
-    queryKey: ['todos'],
-    queryFn: todoService.getTodos,
-  })
-  if (result.isError) return <ErrorMessage text="Failed to load todos." />
-  if (!result.isSuccess) return
-  if (result.data.length > 0) {
-    return (
-      <ul className="px-5 flex flex-col overflow-y-auto">
-        {result.data.map((todo) => (
-          <TodoListItem key={todo.id} todo={todo} />
-        ))}
-      </ul>
-    )
-  } else {
-    return (
-      <div className="grow flex items-center justify-center">
-        <p className="text-neutral-300 text-sm text-center">No todos yet</p>
-      </div>
-    )
-  }
+type TodoListProps = {
+  todos: TodoDto[]
+}
+
+function TodoList(props: TodoListProps) {
+  return (
+    <ul className="px-5 flex flex-col overflow-y-auto">
+      {props.todos.map((todo) => (
+        <TodoListItem key={todo.id} todo={todo} />
+      ))}
+    </ul>
+  )
 }
 
 export default TodoList
