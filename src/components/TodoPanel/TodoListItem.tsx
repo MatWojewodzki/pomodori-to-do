86596a94df9f3468a4cf86b01e6d979dc5a18461
Mateusz Wojewódzki <matwojewodzki@gmail.com>
2026-06-25 12:25:44 +0200

    style: remove the default outline on focus from todo list item

--- src/components/TodoPanel/TodoListItem.tsx
+++ src/components/TodoPanel/TodoListItem.tsx
@@ -38,8 +38,8 @@ function TodoListItem({ todo, index }: TodoListItemProps) {
     <li
       ref={ref}
       className={classNames(
-        'flex items-start gap-2 px-2 py-1 group',
-        'rounded-md hover:bg-neutral-600 focus-within:bg-neutral-600',
+        'flex items-start gap-2 px-2 py-1 group rounded-md',
+        'hover:bg-neutral-600 focus:outline-none focus-within:bg-neutral-600',
         { 'line-through': todo.completed },
         { 'bg-neutral-600': isDragging }
       )}
