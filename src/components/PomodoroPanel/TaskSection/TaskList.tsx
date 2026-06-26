24bd46f619627bcb82e8025f1216e3ece8cd458f
Mateusz Wojewódzki <matwojewodzki@gmail.com>
2026-06-26 13:57:49 +0200

    refactor: automatically switch to the next uncompleted task after timer finish

--- src/components/PomodoroPanel/TaskSection/TaskList.tsx
+++ src/components/PomodoroPanel/TaskSection/TaskList.tsx
@@ -1,12 +1,38 @@
 import TaskListItem from './TaskListItem.tsx'
 import { Timer } from '../../../hooks/useTimer.ts'
 import { TaskDto } from '../../../types/generated/TaskDto.ts'
-import { useEffect, useRef, useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 import { DragDropProvider } from '@dnd-kit/react'
 import { move } from '@dnd-kit/helpers'
 import taskService from '../../../services/tauri/task.ts'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { isSortable } from '@dnd-kit/react/sortable'
+import { TimerType } from '../../../hooks/useTimerType.ts'
+
+function uncompletedTaskPredicate(task: TaskDto) {
+  return !task.completed && task.pomodoro_completed < task.pomodoro_total
+}
+
+function getNextActiveTaskId(tasks: TaskDto[], activeTaskId: string | null) {
+  if (activeTaskId === null) return null
+  const activeTaskIndex = tasks.findIndex((task) => task.id === activeTaskId)
+  const activeTask = tasks[activeTaskIndex]
+  if (
+    !activeTask.completed &&
+    activeTask.pomodoro_total - activeTask.pomodoro_completed > 1
+  )
+    return null
+
+  const prevTasks = tasks.slice(0, activeTaskIndex)
+  const nextTasks = tasks.slice(activeTaskIndex + 1)
+
+  const nextActiveTask =
+    nextTasks.find(uncompletedTaskPredicate) ||
+    prevTasks.find(uncompletedTaskPredicate)
+  if (nextActiveTask) return nextActiveTask.id
+
+  return null
+}
 
 export type TaskListProps = {
   tasks: TaskDto[]
@@ -15,7 +41,12 @@ export type TaskListProps = {
   timer: Timer
 }
 
-function TaskList({ tasks, ...props }: TaskListProps) {
+function TaskList({
+  tasks,
+  activeTaskId,
+  setActiveTaskId,
+  timer,
+}: TaskListProps) {
   const [localTasks, setLocalTasks] = useState<TaskDto[]>(tasks)
   const isDragging = useRef(false)
   const queryClient = useQueryClient()
@@ -27,10 +58,26 @@ function TaskList({ tasks, ...props }: TaskListProps) {
     },
   })
 
+  const updateActiveTask = useCallback(
+    (prevState: TimerType) => {
+      if (prevState !== TimerType.WORK) return
+      const nextActiveTaskId = getNextActiveTaskId(tasks, activeTaskId)
+      if (nextActiveTaskId) {
+        setActiveTaskId(nextActiveTaskId)
+      }
+    },
+    [tasks, activeTaskId, setActiveTaskId]
+  )
+
   useEffect(() => {
     if (isDragging.current) return
     setLocalTasks(tasks)
   }, [tasks])
+
+  const { onTimerFinish } = timer
+  useEffect(() => {
+    return onTimerFinish(updateActiveTask)
+  }, [onTimerFinish, updateActiveTask])
 
   return (
     <DragDropProvider
@@ -63,9 +110,9 @@ function TaskList({ tasks, ...props }: TaskListProps) {
           <TaskListItem
             key={task.id}
             task={task}
-            isActive={task.id === props.activeTaskId}
-            setAsActive={() => props.setActiveTaskId(task.id)}
-            timer={props.timer}
+            isActive={task.id === activeTaskId}
+            setAsActive={() => setActiveTaskId(task.id)}
+            timer={timer}
             index={index}
           />
         ))}
