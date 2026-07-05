1c0d28d2a933fcdca4d422f014d4f6601515a063
Mateusz Wojewódzki <matwojewodzki@gmail.com>
2026-07-05 16:45:01 +0200

    refactor: create todoListService in the frontend

---
+++ src/services/tauri/todoList.ts
@@ -0,0 +1,21 @@
+import { tauriInvoke } from './core.ts'
+import { TodoListDto } from '../../types/generated/TodoListDto.ts'
+
+const todoListService = {
+  async getTodoLists(): Promise<TodoListDto[]> {
+    return await tauriInvoke('get_todos')
+  },
+
+  async createTodoList(args: { title: string }): Promise<TodoListDto> {
+    return await tauriInvoke('create_todo_list', args)
+  },
+
+  async moveTodoList(args: {
+    initialIndex: number
+    newIndex: number
+  }): Promise<void> {
+    return await tauriInvoke('move_todo_list', args)
+  },
+}
+
+export default todoListService
