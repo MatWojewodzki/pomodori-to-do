f4a1126b9fd9bcb77612eee577258f2425ffe4cd
Mateusz Wojewódzki <matwojewodzki@gmail.com>
2026-08-18 11:04:02 +0200

    feature: group current layout under a DesktopLayout component and render it conditionally on larger screens

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 import './main.css'
-import LeftMenu from './components/LeftMenu/LeftMenu.tsx'
-import TodoPanel from './components/TodoPanel/TodoPanel.tsx'
-import PomodoroPanel from './components/PomodoroPanel/PomodoroPanel.tsx'
-import PanelGap from './components/layout/PanelGap.tsx'
 import { useState } from 'react'
-import classNames from 'classnames'
 import todoListService from './services/tauri/todoList.ts'
 import { useQuery } from '@tanstack/react-query'
 import ErrorMessage from './components/common/ErrorMessage.tsx'
+import { useMediaQuery } from 'usehooks-ts'
+import DesktopLayout from './components/layout/DesktopLayout/DesktopLayout.tsx'
 
 function App() {
-  const [todoPanelWidth, setTodoPanelWidth] = useState(400)
   const [openTodoListId, setOpenTodoListId] = useState<string | null>(null)
+
+  const isMobile = useMediaQuery('(max-width: 768px)')
 
   const result = useQuery({
     queryKey: ['todo-lists'],
@@ -21,33 +19,17 @@ function App() {
   if (result.isError) return <ErrorMessage text="Failed to load todo lists." />
   if (!result.isSuccess) return
 
-  const openTodoList =
-    result.data.find((todoList) => todoList.id === openTodoListId) ?? null
-  return (
-    <div
-      className={classNames(
-        'w-screen h-screen flex items-stretch bg-neutral-700 text-white'
-      )}
-    >
-      <LeftMenu
+  if (isMobile) {
+    return <p>Not implemented</p> // TODO
+  } else {
+    return (
+      <DesktopLayout
         todoLists={result.data}
         openTodoListId={openTodoListId}
         setOpenTodoListId={setOpenTodoListId}
       />
-      <div className="grow flex items-stretch overflow-hidden">
-        {openTodoList && (
-          <TodoPanel
-            width={todoPanelWidth}
-            todoLists={result.data}
-            todoList={openTodoList}
-            setOpenTodoListId={setOpenTodoListId}
-          />
-        )}
-        {openTodoList && <PanelGap setTodoPanelWidth={setTodoPanelWidth} />}
-        <PomodoroPanel isTodoPanelOpen={openTodoList !== null} />
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default App
---
+++ src/components/layout/DesktopLayout/DesktopLayout.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+import LeftMenu from '../../LeftMenu/LeftMenu.tsx'
+import TodoPanel from '../../TodoPanel/TodoPanel.tsx'
+import PanelGap from './PanelGap.tsx'
+import PomodoroPanel from '../../PomodoroPanel/PomodoroPanel.tsx'
+import { TodoListDto } from '../../../types/generated/TodoListDto.ts'
+import classNames from 'classnames'
+
+type DesktopLayoutProps = {
+  todoLists: TodoListDto[]
+  openTodoListId: string | null
+  setOpenTodoListId: React.Dispatch<React.SetStateAction<string | null>>
+}
+
+function DesktopLayout(props: DesktopLayoutProps) {
+  const [todoPanelWidth, setTodoPanelWidth] = useState(400)
+  const openTodoList =
+    props.todoLists.find((todoList) => todoList.id === props.openTodoListId) ??
+    null
+  return (
+    <div
+      className={classNames(
+        'w-screen h-screen flex items-stretch bg-neutral-700 text-white'
+      )}
+    >
+      <LeftMenu
+        todoLists={props.todoLists}
+        openTodoListId={props.openTodoListId}
+        setOpenTodoListId={props.setOpenTodoListId}
+      />
+      <div className="grow flex items-stretch overflow-hidden">
+        {openTodoList && (
+          <TodoPanel
+            width={todoPanelWidth}
+            todoLists={props.todoLists}
+            todoList={openTodoList}
+            setOpenTodoListId={props.setOpenTodoListId}
+          />
+        )}
+        {openTodoList && <PanelGap setTodoPanelWidth={setTodoPanelWidth} />}
+        <PomodoroPanel isTodoPanelOpen={openTodoList !== null} />
+      </div>
+    </div>
+  )
+}
+
+export default DesktopLayout
