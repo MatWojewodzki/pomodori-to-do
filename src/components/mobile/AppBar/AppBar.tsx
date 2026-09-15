e426ee79ef3a9455dbff6df8679bedcdad5575a4
Mateusz Wojewódzki <matwojewodzki@gmail.com>
2026-09-15 18:47:32 +0200

    style: adjust spacing inside the AppBar

--- src/components/mobile/AppBar/AppBar.tsx
+++ src/components/mobile/AppBar/AppBar.tsx
@@ -9,10 +9,10 @@ type AppBarProps = {
 function AppBar(props: AppBarProps) {
   return (
     <div className="pt-[env(safe-area-inset-top)]">
-      <div className="px-6 py-3 flex bg-neutral-700">
-        <div className="grow flex items-center gap-6">
-          {props.leading}
-          {props.title}
+      <div className="px-3 py-3 flex bg-neutral-700">
+        <div className="grow flex items-center gap-3">
+          <span>{props.leading}</span>
+          <span>{props.title}</span>
         </div>
         <ul className="flex gap-4">
           {props.actions?.map((action, index) => (
